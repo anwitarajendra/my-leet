db0284d9c5ac531e1c24ea0f1ab35cef469a6fb3
Anwita R Rao <139212247+anwitarajendra@users.noreply.github.com>
2025-09-02 15:59:08 +0530

    Time: 51 ms (50.22%) | Memory: 29.1 MB (48.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,17 +4,13 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        
         for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            
             int l = i + 1, r = n - 1;
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
                     res.push_back({nums[i], nums[l], nums[r]});
-                    
-                    
                     while (l < r && nums[l] == nums[l+1]) l++;
                     while (l < r && nums[r] == nums[r-1]) r--;
                     
