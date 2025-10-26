1a0c115b28c5246977b4e108023b5d82850d441d
Anwita R Rao <139212247+anwitarajendra@users.noreply.github.com>
2025-10-26 20:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (70.05%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.cpp
+++ 3330-modify-the-matrix/modify-the-matrix.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
-    for (int j = 0; j < matrix[0].size(); ++j) {
-        int mx = -1;
-        for (int i = 0; i < matrix.size(); ++i)
-            mx = max(mx, matrix[i][j]);
-        for (int i = 0; i < matrix.size(); ++i)
-            matrix[i][j] = matrix[i][j] == -1 ? mx : matrix[i][j];
-    }
-    return matrix;    
+        int m = matrix.size(), n = matrix[0].size();
+        for(int j = 0; j < n; ++j){
+            int maxi = -1;
+            for(int i = 0; i < m; ++i) maxi = max(maxi , matrix[i][j]);
+            for(int i = 0; i < m; ++i){
+                if(matrix[i][j] == -1) matrix[i][j] = maxi;
+            }
+        }
+        return matrix;
     }
 };
