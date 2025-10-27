b63d7000e4629adf9dc7d5548425025e61704b84
Anwita R Rao <139212247+anwitarajendra@users.noreply.github.com>
2025-10-27 11:47:08 +0530

    Time: 4 ms (18.03%) | Memory: 97.1 MB (99.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,6 @@ public:
         int min=INT_MAX; 
         int op=0;
         int pist=0; 
-
         for(int i=0; i<prices.size(); i++){
             if(prices[i]<min){
                 min=prices[i];
