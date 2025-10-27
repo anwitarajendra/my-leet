a210fadf64c5c37d93d5dbd961bed8e1494612fd
Anwita R Rao <139212247+anwitarajendra@users.noreply.github.com>
2025-10-27 05:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (18.15%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string result;
+        for(char c: s){
+            if(isalnum(c)){
+                result+=tolower(c);
+            }
+        }
+        int i=0, j=result.length()-1;
+        while(i<j){
+            if(result[i]==result[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
